10e9c28dfd8825eb679cf067c68676a151af6973
Elif Savaş <98321352+elifsavas@users.noreply.github.com>
2022-10-22 16:45:31 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import java.util.Scanner ;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int r, aci ;
+        double pi = 3.14, alan ;
+
+        Scanner input = new Scanner(System.in) ;
+
+        System.out.print("Merkez Açı: ");
+        aci = input.nextInt();
+
+        System.out.print("Dairenin Yarıçapı: ");
+        r = input.nextInt() ;
+
+        alan = (pi*(r*r)*aci)/360 ;
+        System.out.println("Daire Dilimi Alanı:" + alan );
     }
 }
